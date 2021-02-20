4949eedf4fb9ea621da2a32da967af904af0b248
danhausen <danihausen@live.com>
2021-02-20 00:21:09 -0300

    main

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text("Daniel"),
+            backgroundColor: Colors.red,
+          ),
+        ),
+      ),
+    );
