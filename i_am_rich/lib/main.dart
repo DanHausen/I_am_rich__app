bb148bac8fc78fc4860561a15dad9303b571adcd
danhausen <danihausen@live.com>
2021-03-02 19:10:22 -0300

    main

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blue,
-          appBar: AppBar(
-            title: Text("Dan Hausen",
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            backgroundColor: Colors.blueGrey[700],
-          ),
-          body: Center(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[700],
+        appBar: AppBar(
+          title:
+              Text("DAN HAUSEN", style: TextStyle(fontWeight: FontWeight.bold)),
+          backgroundColor: Colors.black,
+        ),
+        body: SafeArea(
+          child: Center(
             child: Image(
               image: AssetImage('images/diamond.png'),
             ),
           ),
         ),
-        debugShowCheckedModeBanner: false,
       ),
+      debugShowCheckedModeBanner: false,
     );
+  }
+}
